3aebee2cd478f74e1f758fe90e8c8e69ddd2038e
zhoudongfei <1371105743@qq.com>
2022-04-02 19:19:54 +0800

    集成gin

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello ccc")
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World",
+		})
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		acceptContent := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptContent, "text/html") {
+			c.String(http.StatusNotFound, "404页面")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    http.StatusNotFound,
+				"error_message": "找不到url",
+			})
+		}
+	})
+
+	r.Run(":8080")
 }
