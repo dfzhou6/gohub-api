b28364b3c84c071ed324bb8cfc5e28a72dff27ec
zhoudongfei <1371105743@qq.com>
2022-04-02 20:10:05 +0800

    初始化路由

---
+++ bootstrap/route.go
@@ -0,0 +1,41 @@
+package bootstrap
+
+import (
+	"gohub/routes"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoute(route *gin.Engine) {
+	// 设置路由
+	registerGlobalMiddleware(route)
+
+	routes.RegisterAPIRoutes(route)
+
+	setUp404Handler(route)
+}
+
+func registerGlobalMiddleware(route *gin.Engine) {
+	// 注册中间件
+	route.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setUp404Handler(route *gin.Engine) {
+	// 注册404处理
+	route.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "404页面")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    http.StatusNotFound,
+				"error_message": "找不到url",
+			})
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
+	"gohub/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,25 +10,10 @@ import (
 func main() {
 	r := gin.New()
 
-	r.Use(gin.Logger(), gin.Recovery())
+	bootstrap.SetupRoute(r)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World",
-		})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		acceptContent := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptContent, "text/html") {
-			c.String(http.StatusNotFound, "404页面")
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    http.StatusNotFound,
-				"error_message": "找不到url",
-			})
-		}
-	})
-
-	r.Run(":8080")
+	err := r.Run(":8080")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
