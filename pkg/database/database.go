b4c8a61ae4a235f27870bd161edc67daaa04e14c
zhoudongfei <1371105743@qq.com>
2022-04-07 22:51:44 +0800

    migrate fresh 命令

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"gohub/pkg/config"
 
 	"gorm.io/gorm"
 	gormLogger "gorm.io/gorm/logger"
@@ -24,4 +26,43 @@ func Connect(dbConfig gorm.Dialector, _logger gormLogger.Interface) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func CurrentDatabase() string {
+	return DB.Migrator().CurrentDatabase()
+}
+
+func DeleteAllTables() error {
+	var err error
+	switch config.Get("database.connection") {
+	case "mysql":
+		err = deleteMySQLTables()
+	default:
+		panic(errors.New("database connection not supported"))
+	}
+	return err
+}
+
+func deleteMySQLTables() error {
+	dbName := CurrentDatabase()
+	tables := []string{}
+	err := DB.Table("information_schema.tables").
+		Where("table_schema", dbName).
+		Pluck("table_name", &tables).
+		Error
+	if err != nil {
+		return err
+	}
+
+	DB.Exec("SET foreign_key_checks = 0;")
+
+	for _, table := range tables {
+		err := DB.Migrator().DropTable(table)
+		if err != nil {
+			return err
+		}
+	}
+
+	DB.Exec("SET foreign_key_checks = 1;")
+	return nil
 }
