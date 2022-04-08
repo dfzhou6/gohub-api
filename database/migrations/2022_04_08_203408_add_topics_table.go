aa7a8b3622054898f2a1ac89ace04c52c0b47539
zhoudongfei <1371105743@qq.com>
2022-04-08 20:44:02 +0800

    话题模型和迁移

---
+++ database/migrations/2022_04_08_203408_add_topics_table.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		models.BaseModel
+	}
+
+	type Category struct {
+		models.BaseModel
+	}
+
+	type Topic struct {
+		models.BaseModel
+
+		Title      string `gorm:"type:varchar(255);not null;index"`
+		Body       string `gorm:"type:longtext;not null"`
+		UserID     string `gorm:"type:bigint;not null;index"`
+		CategoryID string `gorm:"type:bigint;not null;index"`
+
+		User     User
+		Category Category
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Topic{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Topic{})
+	}
+
+	migrate.Add("2022_04_08_203408_add_topics_table", up, down)
+}
